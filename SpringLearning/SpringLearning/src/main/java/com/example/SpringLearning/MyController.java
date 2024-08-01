1cde127a671fcc99cfe739044c3994db332a7640
=Chunnu Ji Tiwari <tchunnuji7@gmail.com>
2024-08-01 13:00:51 +0530

    role based access

--- SpringLearning/SpringLearning/src/main/java/com/example/SpringLearning/MyController.java
+++ SpringLearning/SpringLearning/src/main/java/com/example/SpringLearning/MyController.java
@@ -1,5 +1,6 @@
 package com.example.SpringLearning;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class MyController {
     @GetMapping("/hello")
     public String myMethod(){
         return "Hello World";
+    }
+
+    @PreAuthorize(value = "hasRole('dev')")
+    @GetMapping("/userA")
+    public String userAMethod(){
+        return "Hello userA";
+    }
+
+    @PreAuthorize(value = "hasRole('prod')")
+    @GetMapping("/admin")
+    public String myAdmin(){
+        return "Hello admin";
     }
 }
